a67ef5f9a1473a0bf871e648421d1440427c23f5
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-11-09 13:23:49 -0600

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    HeaderComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
